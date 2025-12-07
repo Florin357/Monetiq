31f037d6a302cfc5721ae63885f4afc7d725a886
Florin Mihai <florin@ityes.eu>
2025-12-07 15:20:25 +0200

    Initial Commit

---
+++ monetiq/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  monetiq
+//
+//  Created by Florin Mihai on 07.12.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ monetiqTests/monetiqTests.swift
@@ -0,0 +1,17 @@
+//
+//  monetiqTests.swift
+//  monetiqTests
+//
+//  Created by Florin Mihai on 07.12.2025.
+//
+
+import Testing
+@testable import monetiq
+
+struct monetiqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
